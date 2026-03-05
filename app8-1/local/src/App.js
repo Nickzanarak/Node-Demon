7e611616a63b8ad1f5e67e3fd1ef30e1bfc79823
Nickzanarak <partarwee@gmail.com>
2026-03-05 11:14:39 +0700

    post

--- app8-1/local/src/App.js
+++ app8-1/local/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import React from 'react';
 import RestBasic from './rest-basic';
+import FormGet from './form-get';
+import FormPost from './form-post';
+
 
 export default function App() {
-  return <RestBasic/>
+  return <FormPost/>
 }
